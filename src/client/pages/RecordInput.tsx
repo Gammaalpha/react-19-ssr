112426c34eb91f68e4bc0c9647d6e65bc8d3cb5d
Faiq Malik <faiqmalik2@gmail.com>
2025-09-07 13:32:41 -0400

    styling and controls update

--- src/client/pages/RecordInput.tsx
+++ src/client/pages/RecordInput.tsx
@@ -1,9 +1,69 @@
-import React from "react";
-// import { useAuth } from "../context/AuthContext";
+import {
+  FormGroup,
+  Stack,
+  TextInput,
+  RadioButtonGroup,
+  RadioButton,
+  Button,
+  Dropdown,
+} from "@carbon/react";
+import React, { useRef } from "react";
 // import { useTranslation } from "react-i18next";
 
 const RecordInput: React.FC = () => {
-  return <div>Record Input Form</div>;
+  const formInfo = useRef({
+    description: "",
+    type: "",
+  });
+
+  const handleFormInput = (key: string, value: string | number | undefined) => {
+    formInfo.current = {
+      ...formInfo.current,
+      [key]: value,
+    };
+    console.log(formInfo.current);
+  };
+
+  const handleSubmit = () => {};
+
+  return (
+    <div>
+      <FormGroup
+        legendId="form-group-1"
+        legendText="Simple mongodb entry system with history"
+        style={{
+          maxWidth: "400px",
+        }}
+      >
+        <Stack gap={7}>
+          <Dropdown
+            id="records-dropdown"
+            items={[]}
+            titleText="Records list"
+            label="Select a record from the list"
+          />
+
+          <TextInput
+            id="description"
+            labelText="Description"
+            onChange={(e) =>
+              handleFormInput("description", e.currentTarget.value)
+            }
+          />
+          <RadioButtonGroup
+            // defaultSelected="new"
+            legendText="Entry Type"
+            name="formgroup-default-radio-button-group"
+            onChange={(e) => handleFormInput("type", e)}
+          >
+            <RadioButton id="new" labelText="New" value="NEW" />
+            <RadioButton id="updated" labelText="Updated" value="UPDATED" />
+          </RadioButtonGroup>
+          <Button>Submit</Button>
+        </Stack>
+      </FormGroup>
+    </div>
+  );
 };
 
 export default RecordInput;
